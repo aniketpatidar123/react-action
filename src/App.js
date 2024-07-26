651dd0486592e2bc1b46a4817ebb7c742582e601
Aniket-Patidar <140492542+Aniket-Patidar@users.noreply.github.com>
2024-07-26 23:58:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Aniket patidar can do  any  thing ji
+      Aniket patidar can do  any  thing ji heyt
     </div>
   );
 }
