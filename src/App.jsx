1aecadc336b973abce70ddba09cfd5e38ab63be6
Aniket Patidar <167737531+aniketpatidar123@users.noreply.github.com>
2024-07-27 09:59:01 +0530

    2 try

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello i am aniket 
         </p>
       </div>
       <p className="read-the-docs">
